bc31b0dea9954b7aa9613413b6cb04de9d55a64d
Pedro <pedroivobarretogomes26@gmail.com>
2023-11-05 20:32:18 -0300

    Model Entidade Cliente

--- gestaofinanceiraacademia/src/main/java/br/com/sistemagestaoacademia/gestaofinanceiraacademia/model/Cliente.java
+++ gestaofinanceiraacademia/src/main/java/br/com/sistemagestaoacademia/gestaofinanceiraacademia/model/Cliente.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Cliente {
+    //Entidade Cliente
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
