9fba20be1b250edd5de567740a9e6eef13b20607
Pedro <pedroivobarretogomes26@gmail.com>
2023-11-05 20:38:16 -0300

    repository Admin

--- gestaofinanceiraacademia/src/main/java/br/com/sistemagestaoacademia/gestaofinanceiraacademia/repository/AdminRepository.java
+++ gestaofinanceiraacademia/src/main/java/br/com/sistemagestaoacademia/gestaofinanceiraacademia/repository/AdminRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
  */
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    //Repository Admin
     Optional<Admin> findByNomeAndSenha(String nome, String senha);
 
 
